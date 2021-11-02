18b07675191c3d4ce4bdb346d783000997c8f670
Bravin <bravint@gmail.com>
2021-11-02 19:44:00 +0000

    removed unnecessary entires from data.js

--- data.js
+++ data.js
@@ -3,60 +3,50 @@ const items = [
         id: "001-beetroot",
         name: "beetroot",
         price: 1.80,
-        image: "assets/icons/001-beetroot.svg"
     },
     {
         id: "002-carrot",
         name: "carrot",
-        price: 0.40,
-        image: "assets/icons/002-carrot.svg"
+        price: 0.40
     },
     {
         id: "003-apple",
         name: "apple",
-        price: 0.62,
-        image: "assets/icons/003-apple.svg"
+        price: 0.62
     },
     {
         id: "004-apricot",
         name: "apricot",
-        price: 1.75,
-        image: "assets/icons/004-apricot.svg"
+        price: 1.75
     },
     {
         id: "005-avocado",
         name: "avocado",
-        price: 0.75,
-        image: "assets/icons/005-avocado.svg"
+        price: 0.75
     },
     {
         id: "006-bananas",
         name: "bananas",
-        price: 0.64,
-        image: "assets/icons/006-bananas.svg"
+        price: 0.64
     },
     {
         id: "007-bell-pepper",
         name: "bell-pepper",
-        price: 0.45,
-        image: "assets/icons/007-bell-pepper.svg"
+        price: 0.45
     },
     {
         id: "008-berry",
         name: "berry",
-        price: 2.00,
-        image: "assets/icons/008-berry.svg"
+        price: 2.00
     },
     {
         id: "009-blueberry",
         name: "blueberry",
-        price: 2.00,
-        image: "assets/icons/009-blueberry.svg"
+        price: 2.00
     },
     {
         id: "010-eggplant",
         name: "eggplant",
-        price: 0.69,
-        image: "assets/icons/010-eggplant.svg"
+        price: 0.69
     }
   ];
